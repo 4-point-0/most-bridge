83a596fd457f2f76f90246204f9fae4304f55367
rimatik <slaven@rimatik.hr>
2024-05-10 16:00:56 +0200

    added get_cku_sui_helper_events query

--- src/sufinity-bridge-backend/src/lib.rs
+++ src/sufinity-bridge-backend/src/lib.rs
@@ -1,4 +1,129 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk::api::management_canister::http_request::{
+    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod, HttpResponse, TransformArgs,
+    TransformContext,
+};
+
+use ic_cdk_macros::{self, query, update};
+use serde::{Serialize, Deserialize};
+use serde_json::{self, Value};
+
+#[derive(Serialize, Deserialize)]
+struct Context {
+    bucket_start_time_index: usize,
+    closing_price_index: usize,
+}
+
+#[ic_cdk::update]
+async fn get_ck_sui_helper_events() -> String {
+
+    let host = "https://fullnode.devnet.sui.io";
+    let url = "https://fullnode.devnet.sui.io:443";
+
+    let request_headers = vec![
+        HttpHeader {
+            name: "Host".to_string(),
+            value: format!("{host}:443"),
+        },
+        HttpHeader {
+            name: "User-Agent".to_string(),
+            value: "demo_HTTP_POST_canister".to_string(),
+        },
+        HttpHeader {
+            name: "Idempotency-Key".to_string(),
+            value: "UUID-123456789".to_string(),
+        },
+        HttpHeader {
+            name: "Content-Type".to_string(),
+            value: "application/json".to_string(),
+        },
+    ];
+
+    let json_string : String = "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"suix_queryEvents\",\"params\":[{\"MoveModule\": {\"package\": \"0x838961f43d7f1cb3a8f3c906da14647d353187738d8f2520d7f1dad5d0ab004c\",\"module\": \"ckSuiHelper\"}},null,null,false]}".to_string();
+
+
+    let json_utf8: Vec<u8> = json_string.into_bytes();
+    let request_body: Option<Vec<u8>> = Some(json_utf8);
+
+
+    let context = Context {
+        bucket_start_time_index: 0,
+        closing_price_index: 4,
+    };
+
+    let request = CanisterHttpRequestArgument {
+        url: url.to_string(),
+        max_response_bytes: None, //optional for request
+        method: HttpMethod::POST,
+        headers: request_headers,
+        body: request_body,
+        transform: Some(TransformContext::new(transform, serde_json::to_vec(&context).unwrap())),
+    };
+
+    match http_request(request).await {
+        Ok((response,)) => {
+            let str_body = String::from_utf8(response.body)
+                .expect("Transformed response is not UTF-8 encoded.");
+            ic_cdk::api::print(format!("{:?}", str_body));
+
+            let result: String = format!(
+                "{}. See more info of the request sent at: {}/inspect",
+                str_body, url
+            );
+            result
+        }
+        Err((r, m)) => {
+            let message =
+                format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}");
+            message
+        }
+    }
+
+}
+
+// Strips all data that is not needed from the original response.
+#[query]
+fn transform(raw: TransformArgs) -> HttpResponse {
+
+    let headers = vec![
+        HttpHeader {
+            name: "Content-Security-Policy".to_string(),
+            value: "default-src 'self'".to_string(),
+        },
+        HttpHeader {
+            name: "Referrer-Policy".to_string(),
+            value: "strict-origin".to_string(),
+        },
+        HttpHeader {
+            name: "Permissions-Policy".to_string(),
+            value: "geolocation=(self)".to_string(),
+        },
+        HttpHeader {
+            name: "Strict-Transport-Security".to_string(),
+            value: "max-age=63072000".to_string(),
+        },
+        HttpHeader {
+            name: "X-Frame-Options".to_string(),
+            value: "DENY".to_string(),
+        },
+        HttpHeader {
+            name: "X-Content-Type-Options".to_string(),
+            value: "nosniff".to_string(),
+        },
+    ];
+
+
+    let mut res = HttpResponse {
+        status: raw.response.status.clone(),
+        body: raw.response.body.clone(),
+        headers,
+        ..Default::default()
+    };
+
+    if res.status == 200 {
+
+        res.body = raw.response.body;
+    } else {
+        ic_cdk::api::print(format!("Received an error from coinbase: err = {:?}", raw));
+    }
+    res
 }
